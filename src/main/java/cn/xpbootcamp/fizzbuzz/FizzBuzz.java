89ad1b93c0326b62e2c51d741ab9e36ab9fe14dc
nalansitan <nalansitan@gmail.com>
2020-05-24 23:20:55 +0800

    add normal numbers testing and show test faild

---
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package cn.xpbootcamp.fizzbuzz;
+
+public class FizzBuzz {
+    public String sayNumbers(int num) {
+        return null;
+    }
+}
---
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package cn.xpbootcamp.fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_itself_when_given_normal_numbers(){
+        FizzBuzz fb = new FizzBuzz();
+        Assert.assertEquals("1",fb.sayNumbers(1));
+    }
+
+}
